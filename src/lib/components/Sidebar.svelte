342fd29daebda3b5ca3c84474505280adf9d043d
TerryHung <terryhung@mail.intai.com.tw>
2025-10-17 16:44:55 +0800

    加入sidebar

---
+++ src/lib/components/Sidebar.svelte
@@ -0,0 +1,54 @@
+<script lang="ts">
+  import { X, House, ChartBar, LogOut } from '@lucide/svelte';
+
+  export let isOpen = false;
+  export let onClose: () => void;
+</script>
+
+<!-- 桌機版 -->
+<aside
+  class="hidden md:fixed md:top-13 md:left-0 md:block md:h-[calc(100vh-56px)]
+    md:w-32 md:bg-calico-secondary
+    md:text-calico-text md:dark:border-gray-700 md:dark:bg-dark-secondary
+    md:dark:text-dark-text"
+>
+  <nav class="space-y-2 p-4">
+    <a href="/" class="flex items-center gap-2 rounded p-2 hover:text-gray-500">
+      <House size={18} /> 首頁
+    </a>
+    <a href="/" class="flex items-center gap-2 rounded p-2 hover:text-gray-500">
+      <ChartBar size={18} /> 統計
+    </a>
+    <a href="/" class="flex items-center gap-2 rounded p-2 hover:text-gray-500">
+      <LogOut size={18} /> 登出
+    </a>
+  </nav>
+</aside>
+
+<!-- 手機版 -->
+{#if isOpen}
+  <div
+    class="fixed top-0 left-0 z-50 w-full bg-calico-secondary
+            md:hidden dark:bg-dark-secondary"
+  >
+    <div
+      class="flex items-center justify-between border-b border-gray-200 p-4
+      dark:border-gray-700 dark:text-dark-text"
+    >
+      <button class="flex items-center gap-2 rounded p-2 hover:text-gray-500" on:click={onClose}>
+        <X />
+      </button>
+    </div>
+    <nav class="space-y-2 p-4 dark:text-dark-text">
+      <a href="/" class="flex items-center gap-2 rounded p-2 hover:text-gray-500">
+        <House /> 首頁
+      </a>
+      <a href="/" class="flex items-center gap-2 rounded p-2 hover:text-gray-500">
+        <ChartBar /> 報表
+      </a>
+      <a href="/" class="flex items-center gap-2 rounded p-2 hover:text-gray-500">
+        <LogOut /> 登出
+      </a>
+    </nav>
+  </div>
+{/if}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,87 @@
 <script lang="ts">
-	import AppBar from '$lib/components/AppBar.svelte';
-	import Calendar from '$lib/components/Calendar.svelte';
+  // 引入icon
+  import { Menu, Ham, Sun, Moon, Github } from '@lucide/svelte';
+  import { initTheme, toggleTheme } from '$lib/theme';
+
+  // 引入自訂元件
+  import Calendar from '$lib/components/Calendar.svelte';
+  import Sidebar from '$lib/components/Sidebar.svelte';
+
+  // 是否為深色模式
+  let isDark = false;
+  // 切換主題顏色
+  function changeTheme() {
+    toggleTheme();
+    isDark = document.documentElement.classList.contains('dark');
+  }
+  // 在組件掛載時檢查當前主題
+  import { onMount } from 'svelte';
+  onMount(() => {
+    initTheme();
+    isDark = document.documentElement.classList.contains('dark');
+  });
+
+  let sidebarOpen = false;
+
+  function toggleSidebar() {
+    sidebarOpen = !sidebarOpen;
+  }
 </script>
 
 <div class="min-h-screen bg-calico-bg dark:bg-dark-bg">
-	<AppBar title="Bento Order" />
+  <!-- Appbar -->
+  <nav
+    class="mb-4 bg-calico-secondary
+        px-4 py-3 text-calico-black dark:bg-dark-secondary dark:text-dark-black"
+  >
+    <div class="container mx-auto flex items-center justify-between">
+      <button
+        class="flex cursor-pointer items-center gap-2 hover:text-gray-500
+										md:hidden"
+        on:click={toggleSidebar}
+      >
+        <Menu />
+      </button>
 
-	<div class="mx-auto max-w-4xl">
-		<Calendar />
-	</div>
+      <!-- 左側logo -->
+      <div class="flex items-center gap-2 text-xl font-semibold">
+        <Ham />
+        <span class="text-xl font-semibold">Bento Order</span>
+      </div>
+
+      <!-- 主題顏色切換 -->
+      <div class="flex items-center gap-6">
+        <button
+          on:click={changeTheme}
+          aria-label="切換主題顏色"
+          class="flex cursor-pointer items-center gap-2 hover:text-gray-500"
+        >
+          {#if isDark}
+            <Moon size="24" />
+          {:else}
+            <Sun size="24" />
+          {/if}
+        </button>
+        <a
+          href="https://github.com/th0225/bento-order-f"
+          class="block flex items-center gap-2 hover:text-gray-500"
+          aria-label="GitHub"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Github size="24" />
+        </a>
+      </div>
+    </div>
+  </nav>
+
+  <div class="flex">
+    <Sidebar isOpen={sidebarOpen} onClose={() => (sidebarOpen = false)} />
+
+    <div class="flex-1 md:ml-32">
+      <div class="mx-auto max-w-4xl p-6">
+        <Calendar />
+      </div>
+    </div>
+  </div>
 </div>
